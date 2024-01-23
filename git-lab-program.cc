87fbdad84551ddab1204ee23e98b79047cfbfa9e
Bnicola16 <bn823321@ohio.edu>
2024-01-23 11:54:27 -0500

    "Here it is"

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/* File: git-lab-program.cc
+      Author: <Brennan Nicola>
+        Date: <1/23/24>
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
